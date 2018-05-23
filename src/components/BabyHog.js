7a0da1ba67f0aca31874712748e04aa8a7c13b1b
azeelleeza <leezaluncheon@gmail.com>
2018-05-23 07:39:31 -0400

    Done.

--- src/components/BabyHog.js
+++ src/components/BabyHog.js
@@ -1,62 +1,69 @@
-import React, { Component } from 'react'
-import { Button, Icon } from 'semantic-ui-react'
-import Baby from '../assets/baby-hog.png'
-import SunBaby from '../assets/sun-eyes.png'
-import BlueBaby from '../assets/blue-eyes.png'
-import GlowingBaby from '../assets/glowing-eyes.png'
+import React, { Component } from "react";
+import { Button, Icon } from "semantic-ui-react";
+// import Baby from "../assets/baby-hog.png";
+import SunBaby from "../assets/sun-eyes.png";
+import BlueBaby from "../assets/blue-eyes.png";
+import GlowingBaby from "../assets/glowing-eyes.png";
 
 const imgMapper = {
-  'blue': BlueBaby,
-  'glowing': GlowingBaby,
-  'sun': SunBaby
-}
+  blue: BlueBaby,
+  glowing: GlowingBaby,
+  sun: SunBaby
+};
 
 class BabyHog extends Component {
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-
-    }
+      name: this.props.name,
+      weight: this.props.weight,
+      eyeColor: this.props.eyeColor
+    };
   }
 
-  selectImg() {
-
+  selectImg(hogEye) {
+    return imgMapper[hogEye];
   }
 
   changeWeight(event) {
-    event.preventDefault()
-
+    event.preventDefault();
+    event.target.id === "increase"
+      ? this.setState({ weight: ++this.state.weight })
+      : this.setState({ weight: --this.state.weight });
   }
 
   render() {
+    console.log(this.props);
     return (
-        <li className="hogbabies">
-          <h1>{this.state.name}</h1>
-          <h3>Weight: {this.state.weight}</h3>
-          <h4>Eye Color: {this.state.eyeColor}</h4>
-          <div className="wrap-crap">
-            <Button animated id="increase" onClick={this.changeWeight.bind(this)}>
-              <Button.Content id="increase" visible>Increase Weight</Button.Content>
-              <Button.Content id="increase" hidden>
-                <Icon id="increase" name='plus' />
-              </Button.Content>
-            </Button>
+      <li className="hogbabies">
+        <h1>{this.state.name}</h1>
+        <h3>Weight: {this.state.weight}</h3>
+        <h4>Eye Color: {this.state.eyeColor}</h4>
+        <div className="wrap-crap">
+          <Button animated id="increase" onClick={this.changeWeight.bind(this)}>
+            <Button.Content id="increase" visible>
+              Increase Weight
+            </Button.Content>
+            <Button.Content id="increase" hidden>
+              <Icon id="increase" name="plus" />
+            </Button.Content>
+          </Button>
 
-            <Button animated onClick={this.changeWeight.bind(this)}>
-              <Button.Content visible>Decrease Weight</Button.Content>
-              <Button.Content hidden>
-                <Icon name='minus' />
-              </Button.Content>
-            </Button>
-          </div>
+          <Button animated onClick={this.changeWeight.bind(this)}>
+            <Button.Content visible>Decrease Weight</Button.Content>
+            <Button.Content hidden>
+              <Icon name="minus" />
+            </Button.Content>
+          </Button>
+        </div>
 
-
-          <img src="{/* give correct img source component based on eyecolor prop */}" style={{height: `${this.state.imgHeight}px`}} alt="MasterBlasterJrJr" />
-
-
-        </li>
-    )
+        <img
+          src={this.selectImg(this.props.eyeColor)}
+          style={{ height: `${this.state.imgHeight}px` }}
+          alt="MasterBlasterJrJr"
+        />
+      </li>
+    );
   }
 }
 
